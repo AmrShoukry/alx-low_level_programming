d38f361bb24a4e6648b7442bce179e803caaf1e6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-16 15:58:25 -0800

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print a string into the standard error'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr, "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
