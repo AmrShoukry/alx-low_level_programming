61ad4d6e38c91a41eb6570cc6d8437827fc09f6c
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 04:20:54 -0800

    reversed alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print alphabets reversed'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 122; n <= 97; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
