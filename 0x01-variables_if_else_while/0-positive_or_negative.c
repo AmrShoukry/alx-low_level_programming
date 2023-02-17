8da9e5ccffdad917d9d28a83506545ee8e4a2293
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 03:17:14 -0800

    fixing path

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to check a number'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	if ($n < 0)
+	{
+		printf("$n is negative\n");
+	}
+	else if ($n > 0)
+	{
+		printf("$n is positive\n");
+	}
+	else if ($n == 0)
+	{
+		printf("$n is zero\n");
+	}
+	return (0);
+}
