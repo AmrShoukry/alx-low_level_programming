76049ba824807a429f37c2eaadf11e9aaa39a241
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 04:13:30 -0800

    not eq

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (!(n == 101 || n == 113))
+		{
+			putchar(n);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
