59a178b2cf57b02a90e72d61c8b0cb5f17c2ac0d
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 04:16:40 -0800

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+	return (0);
+}
