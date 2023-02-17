732cd723cb4b6b4f7394c275a7c079cbb8248316
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 04:24:05 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print hexadecimals'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
