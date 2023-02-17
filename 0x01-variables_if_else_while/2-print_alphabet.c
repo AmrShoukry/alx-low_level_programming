170a89f9544b45f10e9b13c0d071ebd9391006ed
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 03:53:38 -0800

    printing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+
+	return (0);
+}
