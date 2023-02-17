dfae73d20bc96199c580362090dd2b62669f7b89
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-17 03:19:48 -0800

    check1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,15 +10,15 @@
 
 int main(void)
 {
-	if ($n < 0)
+	if (n < 0)
 	{
 		printf("$n is negative\n");
 	}
-	else if ($n > 0)
+	else if (n > 0)
 	{
 		printf("$n is positive\n");
 	}
-	else if ($n == 0)
+	else if (n == 0)
 	{
 		printf("$n is zero\n");
 	}
