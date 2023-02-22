07618a90f64c43be26606fae0a08da35c9fdcb52
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-22 06:46:22 -0800

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,6 +8,29 @@
  * Return: Always 0 (Success)
  */
 
+void hours_check(void)
+{
+	if (h < 10)
+	{
+		_putchar('0');
+		_putchar(h + '0');
+	}
+	else
+	{
+		temp = h;
+
+		for (i = 0; i < 2; i++)
+		{
+			ch[i] = (temp % 10) + '0';
+			temp = temp / 10;
+		} 
+		for (i = 1; i >= 0; i--)
+		{
+			_putchar(ch[i]);
+		}
+	}		
+}
+
 void jack_bauer(void)
 {
 	int h, m, i, temp;
@@ -17,25 +40,7 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			if (h < 10)
-			{
-				_putchar('0');
-				_putchar(h + '0');
-			}
-			else
-			{
-				temp = h;
-
-				for (i = 0; i < 2; i++)
-				{
-					ch[i] = (temp % 10) + '0';
-					temp = temp / 10;
-				} 
-				for (i = 1; i >= 0; i--)
-				{
-					_putchar(ch[i]);
-				}
-			}
+			hours_check();
 			
 			_putchar(':');	
 
