3d537a123febee864ca4d07cdcf1434b9e107ba0
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-21 18:51:19 -0800

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,5 @@
 #include "main.h";
-
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -17,7 +17,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i);
+			printf("%i", i);
 			if (i != 98)
 			{
 				_putchar(',');
@@ -29,7 +29,7 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i);
+			printf("%i", i);
 			if (i != 98)
 			{
 				_putchar(',');
