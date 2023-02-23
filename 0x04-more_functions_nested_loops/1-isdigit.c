aff16c97323c6bd15e0bbfcca877b7f9bdcac2c1
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 15:58:01 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *
+ * @c: character
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
