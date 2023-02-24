f1677b10ea442724f15a44b32e0df4da99f4b9f4
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 16:20:31 -0800

    new

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j + '0');
+		}
+
+		_putchar('\n');
+	}
+}
