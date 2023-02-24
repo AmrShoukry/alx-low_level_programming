dda9446e898240a554e7c4a66edc95e1c672b5b4
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 16:00:43 -0800

    task 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: integer1
+ * @b: integer2
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int _islower(int a, int b)
+{
+	return (a * b);
+}
