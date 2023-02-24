72a3eaf48b9fdd5029c6e99a7b08d172a8b40ae3
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 16:17:54 -0800

    new

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
