704ae96fef603a6a96189956ec19ca9caad33bcf
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 16:57:16 -0800

    numbers

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,11 +15,16 @@ void print_diagonal(int n)
 {
 	int i;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
 	for (i = 0; i < n; i++)
 	{
 		_putchar('\\');
 		_putchar(' ');
+		_putchar('\n');
 	}
 
-	_putchar('\n');
 }
