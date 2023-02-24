b76eedf97a0301400953da97d38f97cdaab5ff29
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-23 17:35:17 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ *
+ * Description: 'Main function is used to print alphabets'
+ *
+ * @size: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+
+}
