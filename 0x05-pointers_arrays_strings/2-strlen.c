62e4f9a9f82e8673d3f89323c414ff2a57aff29a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 12:18:26 -0800

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - check the code
+ *
+ * Description: 'Function'
+ *
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+void _strlen(int *s)
+{
+	int counter = 0;
+	char current = '0';
+
+	do
+	{
+		current = s[counter];
+		counter++;
+	} while (current != '\0');
+
+	return counter - 1;
+
+}
