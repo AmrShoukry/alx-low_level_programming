2eec798a3d0917bc2f8e66479323448d1ad5bcb3
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 12:43:53 -0800

    len

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - check the code
+ *
+ * Description: 'Function'
+ *
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+void print_rev(char *str)
+{
+	int i;
+	int counter = 0;
+	char current = str[counter];
+
+	while (current != '\0')
+	{
+		counter++;
+		current = str[counter];
+	}
+
+	for (i = counter - 1; i >= 0; i--)
+	{
+		printf("%c", current[counter];
+	}
+
+	printf("\n");
+}
