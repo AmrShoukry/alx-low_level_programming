fd25ca9726639f82ff637b73c4ebd93e39b4a9f5
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 15:04:27 -0800

    push

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ *
+ * Description: 'Function'
+ *
+ * @dest: string
+ * @src: string
+ *
+ * Return: Always 0.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+
+	return (src);
+}
