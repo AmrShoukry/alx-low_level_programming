e225c4ec6c77b849dde500b55deee3e9e2aa02c8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 12:02:42 -0800

    98

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code
+ *
+ * Description: 'Function'
+ *
+ * @n: pointer
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
