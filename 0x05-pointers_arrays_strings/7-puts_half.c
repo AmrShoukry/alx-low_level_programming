3bce77f45213f3dc12c8a4ee704b5a7adaf4c967
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 14:17:28 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts_half - check the code
+ *
+ * Description: 'Function'
+ *
+ * @str: string
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int counter = 0;
+	char current = str[counter];
+
+	while (current != '\0')
+	{
+		counter++;
+		current = str[counter];
+	}
+
+	for (i = (counter - 1) / 2; i < counter; i++)
+	{
+		printf("%c", current[counter]);
+	}
+
+	printf("\n");
+}
