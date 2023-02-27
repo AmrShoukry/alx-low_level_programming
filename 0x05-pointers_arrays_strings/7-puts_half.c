c13c860779257e4e3736995ca7708357dd709894
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 14:32:15 -0800

    rev

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -34,8 +34,7 @@ void puts_half(char *str)
 	}
 	else
 	{
-		printf("55555");
-		for (i = (counter - 1) / 2; i < counter; i++)
+		for (i = ((counter - 1) / 2); i < counter; i++)
 		{
 			printf("%c", str[i]);
 		}
