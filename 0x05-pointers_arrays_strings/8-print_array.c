eaa0c16ec80f4cd306db83a56ab80c492851d32a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-27 14:54:20 -0800

    push

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code
+ *
+ * Description: 'Function'
+ *
+ * @a: array
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%i, " , *(a + i));
+		}
+		else
+		{
+			printf("%i\n", *(a + i));
+		}
+	}
+}
