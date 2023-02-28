7e5c2991ed4a24525ddea2f7feef93fc7259ccf1
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-02-28 10:42:44 -0800

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,24 +14,23 @@
 int _atoi(char *s)
 {
 	int counter = 0;
-	char current = s[counter];
 	int numberTrue = 0;
 
-	while (current != '\0')
+	while (s[counter] != '\0')
 	{
-		if (numberTrue == 1 && !(current >= 48 && current <= 57))
+		if (numberTrue == 1 && !(s[counter] >= 48 && s[counter] <= 57))
 		{
 			break;
 		}
 
-		else if (numberTrue == 0 && (current >= 48 && current <= 57))
+		else if (numberTrue == 0 && (s[counter] >= 48 && s[counter] <= 57))
 		{
 			if (s[counter - 1] == '-')
 			{
 				printf("-");
 			}
 
-			printf("%c", current);
+			printf("%c", s[counter]);
 
 			numberTrue = 1;
 		}
