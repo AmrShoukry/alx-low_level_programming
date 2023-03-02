61e44073d94b64615b83720f9819940ffe19f205
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-02 10:58:21 -0800

    cpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - check the code
+ *
+ * Description: 'Function'
+ *
+ * @dest: string
+ * @src: string
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int srcCounter = 0;
+	int destCounter = 0;
+	char current = src[srcCounter];
+
+	while (srcCounter < n && current != '\0')
+	{
+		srcCounter++;
+		current = src[srcCounter];
+	}
+
+	for (i = 0; i < srcCounter; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
