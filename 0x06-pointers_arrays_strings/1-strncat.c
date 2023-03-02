730572bbfb70a956624086229e4848fae49b28d0
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-02 05:46:32 -0800

    n

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code
+ *
+ * Description: 'Function'
+ *
+ * @dest: string
+ * @src: string
+ * @n: number
+ *
+ * Return: Always 0.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+	int srcCounter = 0;
+	int destCounter = 0;
+	char current = src[srcCounter];
+
+	while (current != '\0')
+	{
+		srcCounter++;
+		current = src[srcCounter];
+	}
+
+	current = dest[destCounter];
+
+	while (destCounter != 5)
+	{
+		destCounter++;
+		current = dest[destCounter];
+	}
+
+	j = 0;
+
+	for (i = destCounter; i < destCounter + srcCounter; i++)
+	{
+		dest[i] = src[j];
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
