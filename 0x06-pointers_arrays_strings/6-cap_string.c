e85293abb942e2f9ab024d6841b6bb5171892a0c
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-02 12:42:52 -0800

    upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - check the code
+ *
+ * Description: 'Function'
+ *
+ * @string: string
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *string)
+{
+	char separators[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+	int i = 0;
+	int j = 0;
+	int sep = 0;
+
+	while (string[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (string[i] == separators[j])
+			{
+				sep = 1;
+			}
+		}
+
+		if (sep == 1 && string[i] >= 'a' && string[i] <= 'z')
+		{
+			sep = 0;
+			string[i] = string[i] - 32;
+		}
+
+		i++;
+	}
+}
