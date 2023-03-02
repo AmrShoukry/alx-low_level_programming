707d3d8bec7e800a5cfbe79b03cceebbcd5e369d
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-02 06:05:51 -0800

    strn

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,7 +20,7 @@ char *_strncat(char *dest, char *src, int n)
 	int destCounter = 0;
 	char current = src[srcCounter];
 
-	while (srcCounter != n)
+	while (srcCounter < n && current != '\0')
 	{
 		srcCounter++;
 		current = src[srcCounter];
