fdbdc50a901fa2a2bf1ca592a5dd7046f9ff5433
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-03 11:16:41 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * leet - check the code
+ *
+ * Description: 'Function'
+ *
+ * @string: string
+ *
+ * Return: Always 0.
+ */
+
+char *leet(char *string)
+{
+	char AlphaLower[] = {'a', 'e', 'o', 't', 'l'};
+	char AlphaUpper[] = {'A', 'E', 'O', 'T', 'L'};
+	char newNumber = {'4', '3', '0', '7', '1'};
+	int i = 0;
+	int j = 0;
+
+	while (string[i] != '\0')
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (string[i] == AlphaLower[j] || string[i] == AlphaUpper[j])
+			{
+				string[i] = newNumber[j];
+			}
+		}
+
+		i++;
+	}
