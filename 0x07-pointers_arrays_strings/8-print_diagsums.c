30432506b9fb34453d792f1c3a5f5bec396849c6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-07 11:32:25 -0800

    dia

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - check the code
+ *
+ * Description: 'Function'
+ *
+ * @a: array
+ * @size: int
+ *
+ * Return: Always 0.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += a[i][j];
+				sum2 += a[i][j];
+			}
+
+			if (i + j == size - 1)
+			{
+				sum2 += a[i][j];
+			}
+		}
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
