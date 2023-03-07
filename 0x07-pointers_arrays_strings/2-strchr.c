1aaeb4decb548a7b055f7ed4cf0b2911b9d9c059
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-07 05:40:23 -0800

    chr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - check the code
+ *
+ * Description: 'Function'
+ *
+ * @s: string
+ * @c: character
+ *
+ * Return: Always 0.
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+
+		i++;
+	}
+	
+
+	return (NULL);
+}
