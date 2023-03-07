ab5da68a524fc0dfb18fca97cdf3c20aac1c4f5f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-07 11:21:42 -0800

    chess

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - check the code
+ *
+ * Description: 'Function'
+ *
+ * @a: string
+ *
+ * Return: Always 0.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
