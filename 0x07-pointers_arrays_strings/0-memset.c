45d3b50f0339e2c22dc6507582f6cea1b81b15a8
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-07 05:21:55 -0800

    fill

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - check the code
+ *
+ * Description: 'Function'
+ *
+ * @s: string
+ * @b: character
+ * @n: integer
+ *
+ * Return: Always 0.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+
+	return s
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H_
+#define
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
