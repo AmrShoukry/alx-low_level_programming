45eda98a32d1efeb42a778d9a4060d053a546b88
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-07 11:40:48 -0800

    dia

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -25,13 +25,13 @@ void print_diagsums(int *a, int size)
 		{
 			if (i == j)
 			{
-				sum1 += a[i][j];
-				sum2 += a[i][j];
+				sum1 += a[i + j];
+				sum2 += a[i + j];
 			}
 
 			if (i + j == size - 1)
 			{
-				sum2 += a[i][j];
+				sum2 += a[i + j];
 			}
 		}
 	}
