4a0f5439e888708b6837cad71f8bff346cda8a7f
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-08 04:24:54 -0800

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -11,14 +11,14 @@
  * Return: Always 0.
  */
 
-void print_rev_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return;
 	}
 
-	print_rev_recursion(s + 1);
+	_print_rev_recursion(s + 1);
 
 	printf("%c", *s);
 }
