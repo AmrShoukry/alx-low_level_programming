12175dbcb1e3068d65a086e829b515ce7eff3354
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-08 13:28:53 -0800

    wild

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,58 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * wild_checker - check the code
- *
- * Description: 'Function'
- *
- * @s1: string
- * @s2: string
- * @s1_counter: number
- * @s2_counter: number
- *
- * Return: Always 0.
- */
-
-int wild_checker(char *s1, char *s2, int s1_counter, int s2_counter)
-{
-	if (s1[s1_counter] != '\0' || s2[s2_counter] != '\0')
-	{
-		if (s2[s2_counter] == '*')
-		{
-			printf("i'm wild! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-			return (wild_checker(s1, s2, s1_counter, s2_counter + 1));
-		}
-
-		if (s2[s2_counter] != s1[s1_counter])
-		{
-			if (s2[s2_counter - 1] == '*')
-			{
-				printf("previous is wild! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-				return (wild_checker(s1, s2, s1_counter + 1, s2_counter));
-			}
-			else
-			{
-				printf("we are not the same! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-				return (0);
-			}
-		}
-		else
-		{
-			printf("we are matched! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-			return (wild_checker(s1, s2, s1_counter + 1, s2_counter + 1 ));
-		}
-	}
-
-	if (s2[s2_counter] == '\0')
-	{
-		printf("s2 is over! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-		return (1);
-	}
-	
-	printf("s1 and s2 are over! s1:%c, s2:%c\n", s1[s1_counter], s2[s2_counter]);
-	return (0);
-}
 
 /**
  * wildcmp - check the code
@@ -67,5 +15,25 @@ int wild_checker(char *s1, char *s2, int s1_counter, int s2_counter)
 
 int wildcmp(char *s1, char *s2)
 {
-	return (wild_checker(s1, s2, 0, 0));
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+
+	if (*s2 == '*' && *(s2 + 1) != '\0' && *s1 == '\0')
+	{
+		return (0);
+	}
+
+	if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1, s2 + 1) || wildcmp (s1 + 1, s2));
+	}
+
+	return (0);
 }
