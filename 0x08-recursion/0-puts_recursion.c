6a513020818323cca78371b04ece5f6d4ec029fa
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-08 04:15:35 -0800

    r

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - check the code
+ *
+ * Description: 'Function'
+ *
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+char *_puts_recursion(char *s)
+{
+	return (s);
+}
