987afcc8da0597e2c361fd843672f9f486986b5c
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-08 04:29:50 -0800

    len

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,12 +11,12 @@
  * Return: Always 0.
  */
 
-int strlen_recursion(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
 		return (0);
 	}
 
-	return (1 + strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
