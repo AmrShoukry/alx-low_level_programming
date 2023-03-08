dfa5d00add870e548f47b4eb2ff8bf437562a84e
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-08 05:37:18 -0800

    sqrt

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@
 
 int prime_check(int n, int checker)
 {
-	if (n < 0)
+	if (n < 2)
 	{
 		return (0);
 	}
@@ -48,5 +48,5 @@ int prime_check(int n, int checker)
 
 int is_prime_number(int n)
 {
-	return (prime_check(n, 1));
+	return (prime_check(n, 2));
 }
