71eafd6ebb64a1a38cad27eef2f8d25318d3268d
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-10 09:23:30 -0800

    p

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @argc: number
+ * @argv: array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+
+	return (0);
+}
