79311447ff446e3a4785be4498dd0e88cf8bbb89
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-14 14:37:35 -0700

    100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * argstostr - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @ac: number
+ * @av: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *str;
+	int counter = 0;
+	int charCount = 0;
+	int overall = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		str = malloc(sizeof(int) * ac * (* av))
+		while (counter < ac)
+		{
+			while (av[counter][charCount] != '\0')
+			{
+				str[overall] = av[counter][charCount];
+				overall++;
+				charCount++;
+			}
+			charCount = 0;
+			counter++
+
+			str[overall] = '\n';
+			overall++;
+		}
+		str[overall] = '\0';
+	}
+
+	
+}
