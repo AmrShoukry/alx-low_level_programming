7345f1c58decfdc48212dffc24c650a29b01d411
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-14 12:29:20 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	char *strMalloc = malloc(sizeof(char) * size);
+
+	if (str == NULL || strMalloc == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			strMalloc[i] = str[i];
+		}
+		return (strMalloc);
+	}
+}
