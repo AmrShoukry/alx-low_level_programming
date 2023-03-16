6c78ff9177e0d5edbe688ee633f1d1f89228017e
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-16 05:02:52 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @nmemb: number
+ * @size: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < nmemb; i++)
+		{
+			ptr[i] = 0;
+		}
+
+		return (ptr);
+	}
+}
