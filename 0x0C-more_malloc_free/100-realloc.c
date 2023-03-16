6ba0723118b79d936dbf1a06c714e43cc728c1d6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-16 08:01:53 -0700

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,32 +17,35 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	char *newptr;
-	char *oldptr = ptr;
+	char *oldptr;
 
-	if (new_size == 0 && oldptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
 	if (new_size == old_size)
 	{
-		return (oldptr);
+		return (ptr);
 	}
 	if (oldptr == NULL && new_size != 0)
 	{
 		return (malloc(new_size));
 	}
+
+	newptr = malloc(new_size);
+	if (newptr == NULL)
+	{
+		return (NULL);
+	}
+
 	if (old_size < new_size)
 	{
-		newptr = malloc(new_size);
 		for (i = 0; i < old_size; i++)
-		{
 			newptr[i] = oldptr[i];
-		}
 	}
 	if (old_size > new_size)
 	{
-		newptr = malloc(new_size);
 		for (i = 0; i < new_size; i++)
 			newptr[i] = oldptr[i];
 	}
