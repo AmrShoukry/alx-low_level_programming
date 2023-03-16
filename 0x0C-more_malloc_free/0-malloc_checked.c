1ebf656b6254dd71539d18756b06d17f3ef2a9dc
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-16 04:15:27 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @b: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (ptr);
+	}
+}
