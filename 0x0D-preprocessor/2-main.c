4578a16e74ea9083f92535e545d8889e5118cb87
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-20 12:02:25 -0700

    2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to check a number'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
