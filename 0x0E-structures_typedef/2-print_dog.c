526e647e614fa0a923487883f4eaa785061efc1a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-20 13:10:01 -0700

    2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,15 @@ void print_dog(struct dog *d)
 		return;
 	}
 
-	printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-	printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", d->owner = NULL ? "(nil)" : d->owner);
+	if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+
+	if (d=>owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
