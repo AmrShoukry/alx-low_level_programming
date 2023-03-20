d1b5f82c8161c77a7e4109dc9bbe23900b131322
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-20 12:48:18 -0700

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @d: number
+ * @name: number
+ * @age: number
+ * @owner: number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,3 +18,11 @@ struct dog
 };
 
 #endif
+
+
+#ifndef prototypes
+#define prototypes
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
