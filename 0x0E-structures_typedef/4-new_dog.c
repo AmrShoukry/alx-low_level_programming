ecb101382fafc48fa90e9da41068f0dd55982e63
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-20 13:21:30 -0700

    4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,12 +15,14 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *d;
+
 	if (name == NULL || owner == NULL)
 	{
 		return (NULL);
 	}
 
-	dog_t d = malloc(sizeof(struct dog));
+	d = malloc(sizeof(struct dog));
 
 	d->name = name;
 	d->age = age;
