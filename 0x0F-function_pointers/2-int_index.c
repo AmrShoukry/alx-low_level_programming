d4d2f1ab6891bb128bb320fc6a2876f4a5509b07
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-22 07:42:39 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - prints a name as is
+ *
+ * @array: name of the person
+ * @size: f
+ * @cmp: cmp
+ *
+ * Return: Nothing.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	int value;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		value = cmp(array[i]);
+		if (value > 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
