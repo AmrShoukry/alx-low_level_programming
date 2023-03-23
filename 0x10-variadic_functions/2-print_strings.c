b21f01f69fa630e48940b76f273b77f299fb85d1
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-23 03:51:26 -0700

    2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints a name as is
+ *
+ * @separator: s
+ * @n: f
+ *
+ * Return: Nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list ap;
+
+	va_start(ap, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == 0 || separator == NULL)
+		{
+			printf("%s", va_arg(ap, char *));
+		}
+		else
+		{
+			printf("%s%s", separator, va_arg(ap, char *));
+		}
+	}
+
+	printf("\n");
+
+	va_end(ap);
+}
