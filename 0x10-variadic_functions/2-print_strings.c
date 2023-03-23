365b0df288eb1643ebbb08efcd45d460881fe0fc
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-23 03:55:53 -0700

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,9 +20,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == 0 || separator == NULL)
+		if ((i == 0 || separator == NULL) && va_arg(ap, char *) != NULL)
 		{
 			printf("%s", va_arg(ap, char *));
+		}
+		else if (va_arg(ap, char *) == NULL)
+		{
+			continue;
 		}
 		else
 		{
