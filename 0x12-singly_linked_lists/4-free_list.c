cf35e281238577bcf5eaead1ebed534709881a62
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-03-30 06:55:06 -0700

    f

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function
+ *
+ * @head: header
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+
+
+void free_list(list_t *head)
+{
+	list_t temp;
+	temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
