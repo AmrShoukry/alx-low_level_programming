e1bd17e682d31588855504de796d8903617ac2a0
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-03 13:48:34 -0700

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - function
+ *
+ * @head: header
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp = head;
+
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		free(head);
+		head = temp;
+	}
+}
