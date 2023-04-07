6e1ab6a0835e89bf885ca9cb1d75955392030f8e
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-07 15:35:55 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,7 +26,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file_fd = open(filename, O_APPEND);
+	file_fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (file_fd == -1)
 	{
@@ -37,13 +37,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		write_fd = write(file_fd, text_content, strlen(text_content));
 
-		close(file_fd);
-
 		if (write_fd == -1)
 		{
 			return (-1);
 		}
 	}
+
+	close(file_fd);
 
 	return (1);
 
