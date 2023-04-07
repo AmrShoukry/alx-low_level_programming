2e5dfeac66670d868cbc1ce80eeb9ac0ff23eb63
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-07 15:00:28 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,7 +35,6 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	write_fd = write(file_fd, text_content, strlen(text_content));
-	
 
 	if (file_fd == -1 || write_fd == 1 || filename == NULL)
 	{
