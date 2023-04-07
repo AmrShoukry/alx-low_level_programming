8702f7e81b00641011472aa1c5b8b36d1beda067
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-07 16:18:02 -0700

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,82 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Main function is used to print numbers'
+ *
+ * @argc: number
+ * @argv: array
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int fileTo_fd;
+	int fileFrom_fd;
+	int truncate_fd;
+	int closeFileTo_fd;
+	int closeFileFrom_fd;
+	int write_fd;
+	int read_fd;
+	char text[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fileFrom_fd = open(argv[1], O_RDONLY);
+	fileTo_fd = open(argv[2], O_WRONLY);
+
+	if (fileFrom_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (fileTo_fd == -1)
+	{
+		fileTo_fd = creat(argv[2], 0664);
+		if (fileTo_fd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	truncate_fd = truncate(argv[2], 0);
+	if (truncate_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	
+	while ((read_fd = read(fileFrom_fd, text, 1024)) > 0)
+	{
+		write_fd = write(fileTo_fd, text, read_fd);
+
+		if (write_fd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+
+	closeFileFrom_fd = close(fileFrom_fd);
+	closeFileTo_fd = close(fileTo_fd);
+
+	if (closeFileFrom_fd == -1 || closeFileTo_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", -1);
+		exit(100);
+	}
+	return (0);
+}
