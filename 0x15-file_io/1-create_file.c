4170ca3ccfb46042e6db1a59bc6808233edda016
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-07 14:59:24 -0700

    1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+
+/**
+ * create_file - function
+ *
+ * @filename: header
+ * @text_content: letters
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int file_fd;
+	int write_fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (access(filename, F_OK) == -1)
+	{
+		file_fd = creat(filename, 0600);
+	}
+	else
+	{
+		file_fd = open(filename, O_WRONLY);
+	}
+
+	write_fd = write(file_fd, text_content, strlen(text_content));
+	
+
+	if (file_fd == -1 || write_fd == 1 || filename == NULL)
+	{
+		return (-1);
+	}
+	return (1);
+
+}
