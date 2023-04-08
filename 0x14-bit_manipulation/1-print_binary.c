3b85b99d454e1f88d3d6e0eb291c60bfe4ed9bd2
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-08 05:20:44 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+#include <string.h>
+
+/**
+ * print_binary - function
+ *
+ * @n: header
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+
+void print_binary(unsigned long int n)
+{
+	int length = 0;
+	int i = 0;
+	unsigned long int copy = n;
+	unsigned long int digit;
+
+	while (copy != 0)
+	{
+		copy = copy >> 1;
+		length++;
+	}
+	if (n == 0)
+	{
+		_putchar('0');
+		_putchar('\n');
+		return;
+	}
+	
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		digit = (n >> i) & 1;
+		_putchar(digit);
+	}
+	_putchar('\n');
+}
