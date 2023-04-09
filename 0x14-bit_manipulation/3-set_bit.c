0137036a38ee9620dc3a3e8fea53e9d9df069cd6
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-08 17:09:24 -0700

    3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * set_bit - function
+ *
+ * @n: header
+ * @index: num
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 0;
+	unsigned long int number = *n;
+	int remainder;
+	unsigned long int length = 0;
+	int *text;
+
+	if (number == 0)
+	{
+		length = 1;
+	}
+	while (number != 0)
+	{
+		number = number / 2;
+		length++;
+	}
+
+	if (index >= length)
+	{
+		return (-1);
+	}
+
+	number = *n;
+	text = malloc(sizeof(int) * length);
+
+	if (text == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		remainder = number % 2;
+		number = number / 2;
+		*(text + i) = remainder;
+	}
+
+	(*(text + index) = 1);
+
+	return (*(text + index));
+}
