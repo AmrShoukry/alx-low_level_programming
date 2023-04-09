de82df208b9d1917eb6eb4a342fe403e99e91b5a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-08 17:40:20 -0700

    3

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -17,43 +17,43 @@
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i = 0;
-	unsigned long int number = *n;
-	int remainder;
+	unsigned long int i = 0, power = 1, temp = 0, result = 0, number = *n;
 	unsigned long int length = 0;
+	int remainder;
 	int *text;
 
 	if (number == 0)
-	{
 		length = 1;
-	}
 	while (number != 0)
 	{
 		number = number / 2;
 		length++;
 	}
-
 	if (index >= length)
-	{
-		return (-1);
-	}
-
+		length = index + 1;
 	number = *n;
 	text = malloc(sizeof(int) * length);
-
 	if (text == NULL)
-	{
 		return (-1);
-	}
 
 	for (i = 0; i < length; i++)
 	{
+		temp = 0;
+		power = 1;
 		remainder = number % 2;
 		number = number / 2;
 		*(text + i) = remainder;
+		if (i == index)
+		{
+			*(text + index) = 1;
+		}
+		while (temp < i)
+		{
+			power = power * 2;
+			temp++;
+		}
+		result = result + (power * (*(text + i)));
 	}
-
-	(*(text + index) = 1);
-
-	return (*(text + index));
+	*n = result;
+	return (1);
 }
