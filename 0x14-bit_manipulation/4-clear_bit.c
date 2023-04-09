a76722fe712587d69f5ea7b503c5ff792baf351e
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-04-08 17:43:46 -0700

    4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * set_bit - function
+ *
+ * @n: header
+ * @index: num
+ *
+ * Description: print
+ *
+ * Return: Always (0);
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 0, power = 1, temp = 0, result = 0, number = *n;
+	unsigned long int length = 0;
+	int remainder;
+	int *text;
+
+	if (number == 0)
+		length = 1;
+	while (number != 0)
+	{
+		number = number / 2;
+		length++;
+	}
+	if (index >= length)
+		length = index + 1;
+	number = *n;
+	text = malloc(sizeof(int) * length);
+	if (text == NULL)
+		return (-1);
+
+	for (i = 0; i < length; i++)
+	{
+		temp = 0;
+		power = 1;
+		remainder = number % 2;
+		number = number / 2;
+		*(text + i) = remainder;
+		if (i == index)
+		{
+			*(text + index) = 0;
+		}
+		while (temp < i)
