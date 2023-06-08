954963b8dc8315e34b6f08cb371843fb0312e6da
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-08 11:54:34 +0300

    tasks 0, 1, 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * key_index - Function
+ *
+ * Description: 'A function'
+ *
+ * @key: parameter
+ * @size: parameter
+ *
+ * Return: return value
+ */
+
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+    return (hash_djb2(key) % size);
+}
