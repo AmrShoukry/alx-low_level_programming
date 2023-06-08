83209ba4fd1376c727d92ac88e9457b2b12c2ad5
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-06-08 13:05:35 +0300

    -5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Function
+ *
+ * Description: 'A function'
+ *
+ * @ht: parameter
+ *
+ * Return: return value
+ */
+
+
+void hash_table_print(const hash_table_t *ht)
+{
+    int first_ocuurence = 0;
+    long unsigned int i = 0;
+    hash_node_t *node;
+
+    if (ht == NULL)
+        return;
+
+    printf("{");
+
+    while (i < ht->size)
+    {
+        if (ht->array[i] == NULL)
+        {
+            i++;
+            continue;
+        }
+        else
+        {
+            node = ht->array[i];
+            if (first_ocuurence == 0)
+            {
+                first_ocuurence = 1;
+                printf("'%s': '%s' i = %lu\n", node->key, node->value, i);
+                node = node->next;
+            }
+            while (node != NULL)
+            {
+                printf(", '%s': '%s' i = %lu\n", node->key, node->value, i);
+                node = node->next;
+            }
+            i++;
+        }
+    }
+
+    printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,6 +1,9 @@
 #ifndef STRUCTS
 #define STRUCTS
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -41,5 +44,6 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
 
 #endif
