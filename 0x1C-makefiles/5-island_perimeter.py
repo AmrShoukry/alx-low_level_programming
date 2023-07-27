eaa8d3b1f65b6de842018c33f23d95daf1d78168
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-27 14:06:54 +0300

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,24 +4,26 @@
 
 def island_perimeter(grid):
     """
-      calculate permieter of an island 
+      calculate permieter of an island
       first/last? => +number of ones
       Above == 0 || below == 0? => +1
     """
 
-    last_row_index = len(grid) - 1     # get the last row index to get our limits because we are going to test grid[row_index + 1]
-    perimeter = 0                      # our accumulator
-    one_occurence_addition = 0         # if one doesn't occur value => 0; if one occurs value => 2  ((row boundaries))
+    # get the last row index to get our limits
+    # our accumulator
+    # if one doesn't occur value => 0; if one occurs value => 2
+    last = len(grid) - 1
+    perimeter = 0
+    one_occurence_addition = 0
 
-
-    for row_index, row in enumerate(grid):
+    for row_indx, row in enumerate(grid):
         one_occurence_addition = 0
-        for element_index, element in enumerate(row):
+        for elemnt_indx, element in enumerate(row):
             if element == 1:
                 one_occurence_addition = 2
-                if row_index - 1 < 0 or grid[row_index - 1][element_index] == 0:
+                if row_indx - 1 < 0 or grid[row_indx - 1][elemnt_indx] == 0:
                     perimeter += 1
-                if row_index + 1 > last_row_index or grid[row_index + 1][element_index] == 0:
+                if row_indx + 1 > last or grid[row_indx + 1][elemnt_indx] == 0:
                     perimeter += 1
 
         perimeter += one_occurence_addition
